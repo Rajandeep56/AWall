cfcfd81975098a7619ff52324db47ea9ec040686
= <rajandeep.kaur@gov.bc.ca>
2024-02-25 23:21:20 -0800

    Backend full

---
+++ Awall-BackendAPI/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+const router = require('./routes/router');
+
+app.use(express.json());
+
+app.use('/', router);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
