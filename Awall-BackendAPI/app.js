3b66cd892df8326480283cd7a07d3c4eb24ead8b
Rajandeep56 <rajandeepkaur61@outlook.com>
2024-03-24 10:48:06 -0700

    aawall

--- Awall-BackendAPI/app.js
+++ Awall-BackendAPI/app.js
@@ -6,7 +6,7 @@ const router = require('./routes/router');
 
 app.use(express.json());
 
-app.use('/', router);
+app.use('/api', router);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
